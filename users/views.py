c061b5987c8dad274886f7d3c950f80b0c31c37d
manoj <manojsaru258@gmail.com>
2019-12-09 15:39:17 +0545

    flash message

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'your account has been created !  You are now able to login ')
+            messages.success(request, f' your account has been created {username} !  You are now able to login ')
             return redirect('login')
 
     else:
