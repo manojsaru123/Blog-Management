1f65e43fb4bd2df48ea67e97104127f5bef21abe
manoj <manojsaru258@gmail.com>
2019-12-11 11:39:55 +0545

    user delete update

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,10 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+
+class Meta:
+    model = User
+    fields = ['username', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
